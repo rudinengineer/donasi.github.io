42029ff434e81c0861c8160b4b21f0acf81e4426
RudinEngineer <rudinengineer@gmail.com>
2023-12-25 08:56:07 +0700

    deploy awal

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./**/*.html'],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#da336b',
+        dark: '#4a4a4a',
+        light: '#fff',
+        secondary: '#f7f7f7'
+      }
+    },
+  },
+  plugins: [],
+} 
